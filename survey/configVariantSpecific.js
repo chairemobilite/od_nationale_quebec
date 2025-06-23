8a4eb3dfc857ff3a71adc3da35841598af521225
Geneviève Bastien <gbastien@versatic.net>
2025-06-23 10:08:45 -0400

    Première version minimale fonctionnelle du code
    * Ajout du sous-modules d'Evolution
    * Ajout des fichiers de configuration de base eslint, prettier, etc.
    * Ajout des fichiers .gitignore, .env.example et configuration de vscode
    * Ajout d'un Dockerfile
    * Configuration du projet `od_nationale_quebec` dans le répertoire
      `survey` avec un serveur et une app fonctionnelles pour l'application
    participant et admin
    
    Cette version ne contient aucune enquête fonctionnelle

---
+++ survey/configVariantSpecific.js
@@ -0,0 +1,64 @@
+module.exports = {
+    sherbrooke: {
+        trRoutingScenarios: {
+            SE: '4349d0c8-f36e-4924-b087-4f09ff61e6d1',
+            SA: 'fa264668-f9e1-4ef6-8b0a-f79fc128ee7d',
+            DI: '8b1ef717-c5f1-4968-ba8d-e00be4cd5104'
+        },
+        mapDefaultCenter: {
+            lat: 45.40229,
+            lon: -71.88846
+        },
+        mapMaxGeocodingResultsBounds: [{
+            lat: 45.5405672,
+            lng: -71.717510
+        }, {
+            lat: 45.218488,
+            lng: -72.195862
+        }],
+        title: {
+            fr: "Enquête Origine-Destination 2024",
+            en: "2024 Origin-Destination Survey "
+        }
+    },
+    troisrivieresbecancour:{
+        trRoutingScenarios: {
+            SE: 'c1b7451a-e16b-47e5-b10c-e91371dd3217',
+            SA: '793ae097-518e-46ec-b763-e8cca3446b02',
+            DI: '50147472-a7e4-46cc-ad39-0e19198dfe19'
+        },
+        mapDefaultCenter: {
+            lat: 46.34309,
+            lon: -72.54201
+        },
+        mapMaxGeocodingResultsBounds: [{
+            lat: 46.482701,
+            lng: -72.39686
+        }, {
+            lat: 46.1856657,
+            lng: -72.64123
+        }],
+        title: {
+            fr: "Enquête Origine-Destination 2024",
+            en: "2024 Origin-Destination Survey "
+        }
+    },
+    nationale: {
+        mapDefaultZoom: 8,
+        mapDefaultCenter: {
+            lat: 46.19474,
+            lon: -72.81545
+        },
+        mapMaxGeocodingResultsBounds: [{
+            lat: 50.4074347,
+            lng: -65.197417
+        }, {
+            lat: 44.720910,
+            lng: -75.591833
+        }],
+        title: {
+            fr: "Enquête Nationale Origine-Destination 2025",
+            en: "2025 National Origin-Destination Survey "
+        }
+    }
+}
