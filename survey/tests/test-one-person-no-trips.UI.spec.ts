49b6cc80ecb9209e95b44914d262fce1a5228aba
Geneviève Bastien <gbastien@versatic.net>
2025-06-30 13:05:02 -0400

    uiTests: introduce firsts test to test login to the survey
    fixes #10
    
    Add 2 test files: `test-one-person-no-trips` and
    `test-two-persons-no-trips`, with only the login procedure being tested,
    one with an existing access/postal code pair, another one with an
    unexisting pair. The latter logs out from the survey and logs back in to
    its own survey.

---
+++ survey/tests/test-one-person-no-trips.UI.spec.ts
@@ -0,0 +1,49 @@
+import { test } from '@playwright/test';
+import * as testHelpers from 'evolution-frontend/tests/ui-testing/testHelpers';
+import * as surveyTestHelpers from 'evolution-frontend/tests/ui-testing/surveyTestHelpers';
+import { SurveyObjectDetector } from 'evolution-frontend/tests/ui-testing/SurveyObjectDetectors';
+
+const context = {
+    page: null as any,
+    objectDetector: new SurveyObjectDetector(),
+    title: '',
+    widgetTestCounters: {}
+};
+
+// Configure the tests to run in serial mode (one after the other)
+test.describe.configure({ mode: 'serial' });
+
+// Initialize the test page and add it to the context
+test.beforeAll(async ({ browser }) => {
+    context.page = await testHelpers.initializeTestPage(browser, context.objectDetector);
+});
+
+/********** Start the survey **********/
+
+// Start the survey with access code and postal code, the combination does not exist
+const postalCode = 'G1R 5H1';
+const accessCode = '1234-1133';
+surveyTestHelpers.startAndLoginWithAccessAndPostalCodes({
+    context,
+    title: 'Enquête Nationale Origine-Destination 2025',
+    accessCode,
+    postalCode,
+    expectedToExist: false,
+    nextPageUrl: 'survey/home'
+});
+
+// FIXME Implement the rest of the test
+
+// Logout and log back in with same credentials, shoud log in directly
+testHelpers.logoutTest({ context });
+testHelpers.hasConsentTest({ context });
+testHelpers.startSurveyTest({ context });
+testHelpers.registerWithAccessPostalCodeTest({
+    context,
+    postalCode,
+    accessCode,
+    expectedToExist: true,
+    nextPageUrl: 'survey/home'
+});
+
+// FIXME Validate the survey re-entry
