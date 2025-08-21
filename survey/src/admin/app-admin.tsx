dfab29cb2d89570fea90fbaf53ffd524196acb1f
Geneviève Bastien <gbastien@versatic.net>
2025-08-21 15:22:54 -0400

    admin: setup the interviewer roles for the survey
    fixes #129
    
    This adds the interviewer roles, as well as the interviewer's page that
    links to the list of surveys that interviewers can use to search for
    access codes and create new interviews.
    
    This was copied directly from od_nationale_2024

--- survey/src/admin/app-admin.tsx
+++ survey/src/admin/app-admin.tsx
@@ -8,15 +8,23 @@ import runClientApp from 'evolution-frontend/lib/apps/admin';
 import { setApplicationConfiguration } from 'chaire-lib-frontend/lib/config/application.config';
 import appConfig, { EvolutionApplicationConfiguration } from 'evolution-frontend/lib/config/application.config';
 import VisitedPlaceSection from '../survey/sections/visitedPlaces/template';
+import addInterviewerOptions from 'evolution-interviewer/lib/client/services/interviewers/interviewerSupport';
 
 import surveySections from '../survey/sections';
 import { widgets as widgetsConfig } from '../survey/widgetsConfigs';
 
-setApplicationConfiguration<EvolutionApplicationConfiguration>({
-    sections: surveySections,
-    widgets: widgetsConfig as any,
-    allowedUrlFields: ['source', 'accessCode'],
-    templateMapping: { ...appConfig.templateMapping, visitedPlaces: VisitedPlaceSection }
-});
+// TODO This is a workaround to get the links to the user, until some more complete solution is implemented (see https://github.com/chairemobilite/transition/issues/1516)
+const pages = appConfig.pages;
+pages.push({ path: '/interviews', permissions: { Interviews: ['read', 'update'] }, title: 'customLabel:Interviewers' });
+
+setApplicationConfiguration<EvolutionApplicationConfiguration>(
+    addInterviewerOptions({
+        sections: surveySections,
+        widgets: widgetsConfig as any,
+        allowedUrlFields: ['source', 'accessCode'],
+        templateMapping: { ...appConfig.templateMapping, visitedPlaces: VisitedPlaceSection },
+        pages
+    }) as any
+);
 
 runClientApp();
