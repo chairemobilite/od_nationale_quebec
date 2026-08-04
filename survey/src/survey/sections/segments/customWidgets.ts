ed7c5cf248c451d12a5d8dd3e573d8d035ecb86e
Geneviève Bastien <gbastien@versatic.net>
2026-08-04 13:56:59 -0400

    segments: use mode for transit access conditional
    fixes #502
    
    Instead of the modePre, we use a mode list to get the modes that should
    be selected to ask for the transit access mode.

--- survey/src/survey/sections/segments/customWidgets.ts
+++ survey/src/survey/sections/segments/customWidgets.ts
@@ -108,6 +108,18 @@ const howToBusChoices = [
     }
 ];
 
+// These modes are transit modes that require to ask for the access mode to stop
+const transitModesForAccessMode = [
+    'transitBus',
+    'transitRRT',
+    'transitLRRT',
+    'transitRegionalRail',
+    'transitStreetCar',
+    'transitTaxi',
+    'transitFerry',
+    'train',
+    'intercityBus'
+];
 export const segmentHowToBus: WidgetConfig.InputRadioType = {
     type: 'question',
     path: 'howToBus',
@@ -128,7 +140,6 @@ export const segmentHowToBus: WidgetConfig.InputRadioType = {
     },
     conditional: function (interview, path) {
         const segment: any = getResponse(interview, path, null, '../');
-        const modePre = segment ? segment.modePre : null;
         const mode = segment ? segment.mode : null;
 
         const trip = odSurveyHelpers.getActiveTrip({ interview });
@@ -136,7 +147,7 @@ export const segmentHowToBus: WidgetConfig.InputRadioType = {
 
         const isFirst: boolean = segmentsArray[0] === segment;
 
-        return [modePre === 'transit' && isFirst && mode !== 'paratransit', modePre];
+        return [isFirst && transitModesForAccessMode.includes(mode), null];
     },
     validations: function (value, customValue, interview, path, customPath) {
         return [
