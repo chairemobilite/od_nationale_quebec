139e3e52e822363d0344b44db3e8f60e0023dd8c
Samuel Duhaime-Morissette <sduhaimemorissette@gmail.com>
2026-09-15 15:44:45 -0400

    server: enregistrement des modules de config au démarrage Utilisation des fonctions d'enregistrement de modules au lieu de configuration directe au démarrage du serveur, pour que le workerpool puisse avoir accès aux fonctions configurées.
    Export des survey parsers comme default dans le fichier de définition
    des parseurs puisque c'est ce qui est attendu au chargement des
    parseurs.

--- survey/src/admin/serverAdmin.ts
+++ survey/src/admin/serverAdmin.ts
@@ -9,29 +9,32 @@ import path from 'path';
 import router from 'chaire-lib-backend/lib/api/admin.routes';
 import { registerTranslationDir, addTranslationNamespace } from 'chaire-lib-backend/lib/config/i18next';
 import setupServer from 'evolution-backend/lib/apps/admin';
-import { setProjectConfig } from 'evolution-backend/lib/config/projectConfig';
-import serverUpdateCallbacks from '../survey/server/serverFieldUpdate';
-import serverValidations from '../survey/server/serverValidations';
-import roleDefinitions from './server/roleDefinitions';
 import { setupMonitoringView } from './monitoring';
 import adminRoutes from './routes/admin.routes';
-import { surveyObjectParsers } from './parsers';
+import {
+    registerRoleDefinitionsModule,
+    registerServerUpdateCallbacksModule,
+    registerServerValidationsModule,
+    registerSurveyObjectParsersModule
+} from 'evolution-backend/lib/config/serverConfigRegistry';
 
 // TODO Add validation list filter if necessary
 const configureServer = () => {
-    setProjectConfig({
-        serverUpdateCallbacks,
-        serverValidations,
-        roleDefinitions,
-        surveyObjectParsers
-    });
-
     adminRoutes(router); // Load API admin routes
 
     setupMonitoringView(); // Add a monitoring view
 };
 
-setupServer(configureServer);
+registerServerUpdateCallbacksModule(require.resolve('../survey/server/serverFieldUpdate'));
+registerServerValidationsModule(require.resolve('../survey/server/serverValidations'));
+registerRoleDefinitionsModule(require.resolve('./server/roleDefinitions'));
+registerSurveyObjectParsersModule(require.resolve('./parsers'));
+
+setupServer(configureServer).catch((error) => {
+    console.error('Error starting the server: ', error);
+    // eslint-disable-next-line n/no-process-exit
+    process.exit(1);
+});
 
 // FIXME Project directory is for runtime, locales should be in the config file (See #420)
 registerTranslationDir(path.join(__dirname, '../../locales/'));
